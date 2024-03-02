6437a036dd22723e1099b9c1ef3e8a294510b1b2
MJ <za@mm.st>
2024-03-03 10:49:03 +1300

    Matching Reporting improvements (next send to DB)

--- matching.py
+++ matching.py
@@ -1,9 +1,10 @@
 
+from typing import Type
 import logging
 import pandas as pd
 from data_config import EstablishmentID_Program
-from warnings_errors import ValidationNotifier \
-      , MatchingError, MatchingValidationMeta, MatchingDatasetType, build_errors
+from warnings_errors import  add_to_issue_report, IssueLevel,\
+         IssueType, get_outofbounds_issues     
 
 def prep_for_match(ep_df) -> pd.DataFrame:
     ep_df['Program'] = ep_df['ESTABLISHMENT IDENTIFIER'].map(EstablishmentID_Program)
@@ -29,11 +30,7 @@ def get_mask_datefit(row, slack_days=7):
     return after_commencement and before_end_date
 
 
-def match_assessments(ep_atom_df, matching_ndays_slack: int):
-    # Merge the dataframes on SLK and Program
-    # df = pd.merge(episodes_df, atoms_df, how='inner', left_on=[
-    #               'SLK', 'Program'], right_on=['SLK', 'Program'])
-
+def match_dates_assessments_episodes(ep_atom_df, matching_ndays_slack: int):
     # Filter rows where AssessmentDate falls within CommencementDate and EndDate (or after CommencementDate if EndDate is NaN)
     mask = ep_atom_df.apply(get_mask_datefit, slack_days=matching_ndays_slack, axis=1)
 
@@ -42,58 +39,77 @@ def match_assessments(ep_atom_df, matching_ndays_slack: int):
     return filtered_df
 
 
-def check_atom_in_multi_episodes(matched_df:pd.DataFrame,slack_ndays:int):
-  g = matched_df.groupby('SLK_RowKey')['SLK_RowKey'].nunique()
+def check_atom_in_multi_episodes(ep_atom_df:pd.DataFrame,slack_ndays:int):
+  g = ep_atom_df.groupby('SLK_RowKey')['SLK_RowKey'].nunique()
   duplicates = g[g>1]
   if len(duplicates) > 0:  
       # Get the keys for the duplicate rows
     duplicate_keys = duplicates.index
 
     # Filter matched_df to show only rows that match the duplicate keys
-    duplicate_rows_df = matched_df[matched_df.set_index(['SLK', 'RowKey']).index.isin(duplicate_keys)]
+    duplicate_rows_df = ep_atom_df[ep_atom_df.set_index(['SLK', 'RowKey']).index.isin(duplicate_keys)]
     logging.warn(f"ATOM has matches in multiple episodes {duplicate_rows_df}")
     logging.warn(f"Duplicate matches for slack : {slack_ndays}", duplicate_rows_df)
     return duplicate_rows_df
   return None
 
 
-def match_increasing_slack(ep_df:pd.DataFrame, atom_df:pd.DataFrame, max_slack:int=7):
+def not_in_source_df(slk_program_merged:pd.DataFrame
+                     , source_df:pd.DataFrame
+                    #  , vm:Type[ValidationMeta]
+                     , issue_type:IssueType
+                     ,  key:str):
+  not_in_source = source_df[~source_df[key].isin(slk_program_merged[key])]
+  errors_warnings = add_to_issue_report(not_in_source
+                                # , vm
+                                , issue_type
+                                , issue_level=IssueLevel.ERROR)
+  return errors_warnings
+
+def match_by_matching_keys(ep_df, atom_df, matching_keys=['SLK', 'Program']):
+  
+  slk_program_merged = pd.merge(episodes_df, atoms_df, how='inner'
+                                , left_on=matching_keys, right_on=matching_keys)
+  errors_warnings_atom = not_in_source_df(slk_program_merged
+                                          , atom_df
+                                          , issue_type=IssueType.FOUND_ONLY_IN_ATOM                           
+                                          , key='SLK_RowKey')
+  errors_warnings_ep = not_in_source_df(slk_program_merged
+                                          , ep_df
+                                          , issue_type=IssueType.FOUND_ONLY_IN_EPISODE
+                                          , key='SLK_Program')
+    
+  errors_warnings_atom.extend(errors_warnings_ep)
+  return slk_program_merged, errors_warnings_atom
+
+
+def match_increasing_slack(slk_program_matched:pd.DataFrame, max_slack:int=7):
   matching_ndays_slack = 0 
-  unmatched_atoms = atom_df
+  unmatched_by_date = slk_program_matched
   result_matched_dfs = []
   # atom_df['SLK_RowKey'] =  atom_df['SLK'] + '_' + atom_df['RowKey']
-  atom_df['SLK_RowKey'] =  atom_df['SLK'] + '_' + atom_df['RowKey']
-  ep_df['SLK_Program'] =  ep_df['SLK'] + '_' + ep_df['Program']
-  errors_warnings:list[ValidationNotifier] = []
-
-  slk_program_matched = pd.merge(ep_df, atoms_df, how='inner', left_on=[
-                        'SLK', 'Program'], right_on=['SLK', 'Program'])
-  not_matched_atom = atom_df[~atom_df.SLK_RowKey.isin(slk_program_matched.SLK_RowKey)]
+  # atom_df['SLK_RowKey'] =  atom_df['SLK'] + '_' + atom_df['RowKey']
+  # ep_df['SLK_Program'] =  ep_df['SLK'] + '_' + ep_df['Program']
   
-  ew1 = build_errors(not_matched_atom, MatchingDatasetType.ATOM)
-  not_matched_ep = ep_df[~ep_df.SLK_Program.isin(slk_program_matched.SLK_Program)]
-  ew2 = build_errors(not_matched_ep, MatchingDatasetType.EPISODE)
 
-  ew1.extend(ew2)
-  errors_warnings = ew1
-  while matching_ndays_slack <= max_slack:
+  # program_matched_slk_rks = slk_program_matched.SLK_RowKey
+
+  while len(unmatched_by_date) > 0  and matching_ndays_slack <= max_slack:
       # Get matched assessments with the current slack
-      matched_df = match_assessments(slk_program_matched, matching_ndays_slack)
+      matched_df = match_dates_assessments_episodes(unmatched_by_date, matching_ndays_slack)
       duplicate_rows_df = check_atom_in_multi_episodes(matched_df, matching_ndays_slack)
-      
-
+      if duplicate_rows_df:
+        logging.error("Duplicate rows", duplicate_rows_df)
       if len(matched_df) == 0: # no more SLK+Program matches between Episode and ATOM
-        result_matched_df = pd.concat(result_matched_dfs, ignore_index=True)
-        unmatched_atoms = unmatched_atoms[~unmatched_atoms.SLK_RowKey.isin(atom_df.SLK_RowKey)]
-        return result_matched_df, unmatched_atoms, errors_warnings
+         break
+        # result_matched_df = pd.concat(result_matched_dfs, ignore_index=True)
+        # unmatched_by_date = unmatched_by_date[~unmatched_by_date.SLK_RowKey.isin(program_matched_slk_rks)]
+        # return result_matched_df, unmatched_by_date, errors_warnings
       
       # Add the matched DataFrame to the list
       result_matched_dfs.append(matched_df)
 
-      ## Update unmatched_atoms by filtering out matched SLKs from the current unmatched_atoms
-      ## matched_slks = matched_df.SLK.unique()
-      
-      unmatched_atoms = unmatched_atoms[~unmatched_atoms.SLK_RowKey.isin(matched_df.SLK_RowKey)]
+      unmatched_by_date = unmatched_by_date[~unmatched_by_date.SLK_RowKey.isin(matched_df.SLK_RowKey)]
 
       ## there may be other assessments for this SLK that can match if the slack dways are increased
       ## don't exclude the SLK, but the SLK +RowKey
@@ -101,17 +117,23 @@ def match_increasing_slack(ep_df:pd.DataFrame, atom_df:pd.DataFrame, max_slack:i
       # Increment the slack days for the next iteration
       matching_ndays_slack += 1
 
-  if len(unmatched_atoms) > 0 :
-     logging.info(f"There are still {len(unmatched_atoms)} unmatched ATOMs")
-     logging.info(f"Unmatched by program: {len(unmatched_atoms.Program.value_counts())}")
+  if len(unmatched_by_date) > 0 :
+     logging.info(f"There are still {len(unmatched_by_date)} unmatched ATOMs")
+     logging.info(f"Unmatched by program: {len(unmatched_by_date.Program.value_counts())}")
     #  logger.info(f"There are still {len(unmatched_atoms)} unmatched ATOMs")
     #  logger.info(f"Unmatched by program: {len(unmatched_atoms.Program.value_counts())}")
 
   # Concatenate all matched DataFrames from the list
   result_matched_df = pd.concat(result_matched_dfs, ignore_index=True)
-  return result_matched_df, unmatched_atoms, errors_warnings
+
+  # add_to_issue_report(unmatched_by_date, IssueType.DATE_MISMATCH, IssueLevel.ERROR)
+  return result_matched_df, unmatched_by_date
 
 
+def merge_key(df1, field1, field2):
+  df = df1.copy()
+  df[f'{field1}_{field2}'] =  df[field1] + '_' + df[field2]
+  return df
 
 if __name__ == "__main__":
   # from datetime import datetime
@@ -136,9 +158,7 @@ if __name__ == "__main__":
         #  'GHI',        #  'JKL',        #  'MNO',        #  'PQR'
       ],
       'Program':['TSS',  'TSS', 'SHAWS'],
-
       'CommencementDate': pd.to_datetime(['2023-07-01', '2023-10-01',  '2022-11-01']),
-
       'EndDate': [pd.to_datetime('2023-12-01'), pd.Timestamp.today(), pd.to_datetime('2023-07-01')],       
   })
   
@@ -146,9 +166,18 @@ if __name__ == "__main__":
   # convert_to_datetime(episodes_df,['CommenencementDate','EndDate'])
 
   # matched_df = match_assessments(episodes_df, atoms_df,0)
-  result_matched_df, unmatched_atoms, errors_warnings = match_increasing_slack(episodes_df, atoms_df,1)
+  atoms_df = merge_key(atoms_df, 'SLK', 'RowKey')
+  episodes_df = merge_key(episodes_df, 'SLK', 'Program')
+
+  slk_program_merged, errors_warnings_matchkey = match_by_matching_keys(episodes_df, atoms_df
+                                                               ,matching_keys=['SLK', 'Program'])
+
+  result_matched_df, unmatched_atoms = match_increasing_slack(slk_program_merged,1)
+  date_matched_errwarns = get_outofbounds_issues(unmatched_atoms)
+  
   print(unmatched_atoms)
-  print (errors_warnings)
+  print (errors_warnings_matchkey)
+  print (date_matched_errwarns)
   # from utils.df_ops_base import  get_lr_mux_unmatched
   # left_non_matching, right_non_matching, common_left, common_right = get_lr_mux_unmatched(episodes_df, atoms_df,merge_cols=['SLK', 'Program'])
   # print('left non ' , left_non_matching)
--- warnings_errors.py
+++ warnings_errors.py
@@ -1,29 +1,40 @@
 from datetime import datetime
+from abc import ABC
 from dataclasses import dataclass
 from enum import Enum, auto
 from typing import Protocol, Optional
 
 
-class NotificationType(Enum):
+class IssueLevel(Enum):
   WARNING = auto()
   ERROR = auto()
+
+class IssueType(Enum):
+  DATE_MISMATCH = auto()
+  FOUND_ONLY_IN_ATOM = auto()
+  FOUND_ONLY_IN_EPISODE = auto()
 
 class MatchingDatasetType(Enum):
   ATOM = auto()
   EPISODE = auto()
   NOT_APPLICABLE = auto()
 
-@dataclass(kw_only=True)
-class ValidationMeta:
+@dataclass()#kw_only=True)
+class ValidationMeta(ABC):
   client_id:str # SLK
+ 
+
+@dataclass(kw_only=True)
+class EpisodeValidationMeta(ValidationMeta):
+  program:Optional[str] ="" 
   episode_id:Optional[str] =""
   episode_start:Optional[datetime]= datetime.now()
   episode_end:Optional[datetime]= datetime.now()
-  assessment_date:Optional[datetime]= datetime.now()
 
 @dataclass(kw_only=True)
-class MatchingValidationMeta(ValidationMeta):
-  found_only_in:Optional[MatchingDatasetType] = MatchingDatasetType.NOT_APPLICABLE
+class AssessmentValidationMeta(ValidationMeta):
+  row_key:Optional[str] =""
+  assessment_date:Optional[datetime]= datetime.now()
 
 
 class ValidationNotifier(Protocol):
@@ -38,64 +49,95 @@ class ValidationNotifier(Protocol):
   #   ...
 
 @dataclass(kw_only=True)
-class MatchingWarning:
-  meta: MatchingValidationMeta
-  notification_type:NotificationType = NotificationType.WARNING
+class MatchingIssue:
+  meta: ValidationMeta
+  issue_level:IssueLevel #= NotificationType.WARNING
+  issue_type:IssueType
+  msg:Optional[str]=""
 
   def get_meta(self) -> ValidationMeta:
     return self.meta
 
   def notify(self) -> str:
-    return f"{self.notification_type.name}: "
-  
-
-@dataclass(kw_only=True)
-class MatchingError:
-  meta: MatchingValidationMeta
-  notification_type:NotificationType = NotificationType.ERROR
-
-  def get_meta(self) -> ValidationMeta:
-    return self.meta
-  
-  def notify(self) -> str:
-    return f"{self.notification_type.name}: "
+    return f"{self.issue_level.name}: "
   
 
 
+# from typing import Type
 import pandas as pd
 
-# def build_errors_warnings(data:pd.DataFrame
-#                  , dataset_type:MatchingDatasetType
-#                  , Notifier:ValidationNotifier) -> list:
-#   ew:list= []
-#   for idx, item in data.iterrows():
-#      m = MatchingValidationMeta(client_id=item.SLK
-#                                 , assessment_date=item.AssessmentDate
-#                                 , found_only_in=dataset_type)
-#      ew.append(Notifier(meta=m))  
-     
-#   return ew
 
-def build_errors(data:pd.DataFrame
-                 , dataset_type:MatchingDatasetType
+def add_to_issue_report(data:pd.DataFrame 
+                , issue_type:IssueType
+                , issue_level:IssueLevel
+                , msg:Optional[str]=""      
                  ) -> list:
-  errors:list= []
-  if dataset_type == MatchingDatasetType.ATOM:
-    for idx, item in data.iterrows():
-      m = MatchingValidationMeta(client_id=item.SLK
-                                 #, # TODO add Row_Key
-                                  , assessment_date=item.AssessmentDate
-                                  , found_only_in=dataset_type)
-      #  errors.append(Notifier(meta=m))  
-      errors.append(MatchingError(meta=m))
-  elif dataset_type == MatchingDatasetType.EPISODE:
-    for idx, item in data.iterrows():
-      m = MatchingValidationMeta(client_id=item.SLK
-                                  , episode_start=item.CommencementDate
-                                  , episode_end=item.EndDate
-                                  # , # TODO : add episode ID
-                                  , found_only_in=dataset_type)
-      errors.append(MatchingError(meta=m)) 
+  
+  if 'CommencementDate' in data.columns:
+    metas = [EpisodeValidationMeta(client_id=item.SLK
+                                    , program=item.Program
+                                , episode_start=item.CommencementDate
+                                , episode_end=item.EndDate)
+              for idx, item in data.iterrows()]
   else:
-    raise NotImplementedError
-  return errors
+    metas = [AssessmentValidationMeta(client_id=item.SLK
+                                    , row_key=item.RowKey
+                           , assessment_date=item.AssessmentDate)
+             for idx, item in data.iterrows()]
+  
+  issues = [MatchingIssue(meta=m, issue_type=issue_type
+                       ,issue_level=issue_level, msg=msg)
+            for m in metas]
+
+  return issues
+
+
+
+def gap_ew(df:pd.DataFrame, i_type:IssueType,\
+            i_level:IssueLevel, msg:str="")-> list[MatchingIssue]:
+  metas = [AssessmentValidationMeta(client_id=item.SLK
+                                  , row_key=item.RowKey
+                                , assessment_date=item.AssessmentDate)
+              for idx, item in df.iterrows()]
+
+  issues = [MatchingIssue(meta=m, issue_type=i_type
+                       ,issue_level=i_level, msg=msg)
+            for m in metas]
+  return issues
+
+def gap_asesmtdate_epsd_boundaries(merged_df1:pd.DataFrame):
+  merged_df = merged_df1.copy()
+    # Calculating the difference in days
+  merged_df.loc[:,'days_from_start'] = \
+    (merged_df['AssessmentDate'] - merged_df['CommencementDate']).dt.days
+  merged_df.loc[:,'days_from_end'] = \
+    (merged_df['AssessmentDate'] - merged_df['EndDate']).dt.days
+  return merged_df
+
+
+def get_outofbounds_issues(unmatched_df:pd.DataFrame, limit_days:int = 1):
+  gaps_df = gap_asesmtdate_epsd_boundaries(unmatched_df)
+  # Warning if assessment is within 3 days outside the episode boundaries
+  warning_mask = ((gaps_df['days_from_start'] < 0) & \
+                      (gaps_df['days_from_start'] >= -limit_days)) | \
+                  ((gaps_df['days_from_end'] > 0) & \
+                      (gaps_df['days_from_end'] <= limit_days))
+  # gaps_df.loc[warning_mask, 'status'] = 'Warning'
+  # Error if assessment date is more than 3 days out of the episode boundaries
+  error_mask = (gaps_df['days_from_start'] < -limit_days) | \
+                  (gaps_df['days_from_end'] > limit_days)
+  # gaps_df.loc[error_mask, 'status'] = 'Error'
+
+  message = f"Assessment is out of Episode boundaries by more than {limit_days}."
+
+  warnings = gaps_df[warning_mask]  
+  i_warns = gap_ew(warnings
+                   , i_type=IssueType.DATE_MISMATCH
+                   , i_level=IssueLevel.WARNING, msg=message)
+
+  errors = gaps_df[error_mask]  
+  i_errs = gap_ew(errors
+                  , i_type=IssueType.DATE_MISMATCH
+                  , i_level=IssueLevel.ERROR, msg=message)
+  i_warns.extend(i_errs)
+  return i_warns 
